21287f770fae7abe76cf943bd4883e93d603f53c
Abdullah Al Ashrafee <dev.ashrafee@gmail.com>
2025-10-13 22:21:24 +0600

    padding added

--- Frontend/src/layout/MainWidth.jsx
+++ Frontend/src/layout/MainWidth.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const MainWidth = ({ children }) => {
-  return <div className="max-w-[1600px] mx-auto">{children}</div>;
+  return <div className="max-w-[1580px] px-5 mx-auto">{children}</div>;
 };
 
 export default MainWidth;
