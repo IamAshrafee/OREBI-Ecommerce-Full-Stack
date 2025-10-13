bf464271a2394e8d5db59dfee173d7f73794b3ce
Abdullah Al Ashrafee <dev.ashrafee@gmail.com>
2025-10-13 13:42:27 +0600

    React router installed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Home from "./pages/Home";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 const App = () => {
   return (
     <div>
-      <Home></Home>
+      <RouterProvider router={router} />
     </div>
   );
 };
