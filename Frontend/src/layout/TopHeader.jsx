0da7ae5e6aea9eb8313bed8fe32241ce096825b1
Abdullah Al Ashrafee <dev.ashrafee@gmail.com>
2025-10-14 13:43:39 +0600

    Top header converted to sticky header

--- Frontend/src/layout/TopHeader.jsx
+++ Frontend/src/layout/TopHeader.jsx
@@ -9,58 +9,60 @@ const TopHeader = () => {
   const inactiveStyle =
     "font-dmsans text-gray-v1 hover:text-primary cursor-pointer";
   return (
-    <div>
-      <MainWidth>
-        <div className="flex flex-row justify-between items-center h-20">
-          {/* Logo */}
-          <Link to="/">
-            <img src={Logo} alt="Logo" className="cursor-pointer" />
-          </Link>
-          {/* Menu Items */}
-          <div className="flex flex-row gap-10">
-            <Link
-              to="/"
-              className={
-                location.pathname === "/" ? activeStyle : inactiveStyle
-              }
-            >
-              Home
+    <div className="relative w-full h-20">
+      <div className="fixed top-0 z-50 w-full bg-white ">
+        <MainWidth>
+          <div className="flex flex-row justify-between items-center h-20">
+            {/* Logo */}
+            <Link to="/">
+              <img src={Logo} alt="Logo" className="cursor-pointer" />
             </Link>
-            <Link
-              to="/shop"
-              className={
-                location.pathname === "/shop" ? activeStyle : inactiveStyle
-              }
-            >
-              Shop
-            </Link>
-            <Link
-              to="/about"
-              className={
-                location.pathname === "/about" ? activeStyle : inactiveStyle
-              }
-            >
-              About
-            </Link>
-            <Link
-              to="/contact"
-              className={
-                location.pathname === "/contact" ? activeStyle : inactiveStyle
-              }
-            >
-              Contact
-            </Link>
-            <Link
-              to="/journal"
-              className="font-dmsans text-gray-v1 hover:text-primary cursor-pointer"
-            >
-              Journal
-            </Link>
+            {/* Menu Items */}
+            <div className="flex flex-row gap-10">
+              <Link
+                to="/"
+                className={
+                  location.pathname === "/" ? activeStyle : inactiveStyle
+                }
+              >
+                Home
+              </Link>
+              <Link
+                to="/shop"
+                className={
+                  location.pathname === "/shop" ? activeStyle : inactiveStyle
+                }
+              >
+                Shop
+              </Link>
+              <Link
+                to="/about"
+                className={
+                  location.pathname === "/about" ? activeStyle : inactiveStyle
+                }
+              >
+                About
+              </Link>
+              <Link
+                to="/contact"
+                className={
+                  location.pathname === "/contact" ? activeStyle : inactiveStyle
+                }
+              >
+                Contact
+              </Link>
+              <Link
+                to="/journal"
+                className="font-dmsans text-gray-v1 hover:text-primary cursor-pointer"
+              >
+                Journal
+              </Link>
+            </div>
+            {/* Blank Space */}
+            <div></div>
           </div>
-          {/* Blank Space */}
-          <div></div>
-        </div>
-      </MainWidth>
+        </MainWidth>
+      </div>
     </div>
   );
 };
