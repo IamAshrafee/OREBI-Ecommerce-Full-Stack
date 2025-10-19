c1684b677e03694bcb5e0cbb3c356f19a5af3fd2
Abdullah Al Ashrafee <dev.ashrafee@gmail.com>
2025-10-19 12:31:18 +0600

    About and shop page header created

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import MainWidth from "../layout/MainWidth";
+import Heading1 from "../components/Headings/Heading1";
+import CustomBreadcrumb from "../components/CustomBreadcrumb";
 
-// This is the About page - where will be the infomation about this E-commerce website / about the business 
+// This is the About page - where will be the infomation about this E-commerce website / about the business
 
 const About = () => {
+  const breadCrumbItems = [{ path: "/", label: "Home" }, { label: "About" }];
   return (
-    <div>About</div>
-  )
-}
+    <div className="grow mt-14 mb-28">
+      <MainWidth>
+        <div className="flex flex-col">
+          <div className="mb-20">
+            <Heading1 text="About" />
+            <CustomBreadcrumb items={breadCrumbItems} />
+          </div>
+        </div>
+      </MainWidth>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- Frontend/src/pages/shop/Shop.jsx
+++ Frontend/src/pages/shop/Shop.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import MainWidth from "../../layout/MainWidth";
+import Heading1 from "../../components/Headings/Heading1";
+import CustomBreadcrumb from "../../components/CustomBreadcrumb";
 
-// Shop page - A general page, where will be shown all the products. With some features like searching, sorting products, and filtering out with several options like color, category etc 
+// Shop page - A general page, where will be shown all the products. With some features like searching, sorting products, and filtering out with several options like color, category etc
 
 const Shop = () => {
+  const breadCrumbItems = [{ path: "/", label: "Home" }, { label: "Products" }];
   return (
-    <div>Shop</div>
-  )
-}
+    <div className="grow mt-14 mb-28">
+      <MainWidth>
+        <div className="flex flex-col">
+          <div className="mb-20">
+            <Heading1 text="Products" />
+            <CustomBreadcrumb items={breadCrumbItems} />
+          </div>
+        </div>
+      </MainWidth>
+    </div>
+  );
+};
 
-export default Shop
+export default Shop;
