dbc2135478f48c6321efa07ad88a8b0dd3e4c9ef
Abdullah Al Ashrafee <dev.ashrafee@gmail.com>
2025-10-22 16:37:00 +0600

    inter font installed, and dashboard state cards added

--- Admin Frontend/src/pages/Dashboard.jsx
+++ Admin Frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+const stats = [
+  { name: 'Revenue', value: '$405,091.00', change: '+4.75%', changeType: 'positive' },
+  { name: 'Overdue invoices', value: '$12,787.00', change: '+54.02%', changeType: 'negative' },
+  { name: 'Outstanding invoices', value: '$245,988.00', change: '-1.39%', changeType: 'positive' },
+  { name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative' },
+]
 
-const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
 }
 
-export default Dashboard
+export default function Dashboard() {
+  return (
+    <dl className="mx-auto grid grid-cols-1 gap-px  sm:grid-cols-2 lg:grid-cols-4 ">
+      {stats.map((stat) => (
+        <div
+          key={stat.name}
+          className="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 px-4 py-10 sm:px-6 xl:px-8  shadow-sm shadow-black rounded-[16px]"
+        >
+          <dt className="text-sm/6 font-medium text-gray-500 dark:text-gray-400">{stat.name}</dt>
+          <dd
+            className={classNames(
+              stat.changeType === 'negative' ? 'text-rose-600 dark:text-rose-400' : 'text-gray-700 dark:text-gray-300',
+              'text-xs font-medium',
+            )}
+          >
+            {stat.change}
+          </dd>
+          <dd className="w-full flex-none text-3xl/10 font-medium tracking-tight text-gray-900 dark:text-white">
+            {stat.value}
+          </dd>
+        </div>
+      ))}
+    </dl>
+  )
+}
