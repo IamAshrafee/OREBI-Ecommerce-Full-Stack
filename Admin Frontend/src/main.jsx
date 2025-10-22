f64fd07e6bd6aec27b9848f51723ebff61a95336
Abdullah Al Ashrafee <dev.ashrafee@gmail.com>
2025-10-22 12:59:30 +0600

    Pages created, and routing implemented in main.jsx

--- Admin Frontend/src/main.jsx
+++ Admin Frontend/src/main.jsx
@@ -5,26 +5,47 @@ import App from "./App.jsx";
 import RootLayout from "./RootLayout.jsx";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import { Notifications } from "./pages/Notifications";
+import Products from "./pages/Product/Products.jsx";
+import Orders from "./pages/order/Orders.jsx";
+import CreateOrder from "./pages/order/CreateOrder.jsx";
+import { CreateProduct } from "./pages/Product/CreateProduct.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import MyProfile from "./pages/profile/MyProfile.jsx";
+import Logout from "./pages/profile/Logout.jsx";
+import Login from "./pages/profile/Login.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     children: [
-      { index: true, element: <App /> },
+      { index: true, element: <Dashboard /> },
       {
         path: "/notifications",
-        element: <h1>This is the notification page</h1>,
+        element: <Notifications />,
       },
-      { path: "/products", element: <h1>This is the product page</h1> },
+      { path: "/products", element: <Products /> },
       {
         path: "/create-product",
-        element: <h1>This is the product create page</h1>,
+        element: <CreateProduct />,
       },
-      { path: "/orders", element: <h1>This is the orders page</h1> },
+      { path: "/orders", element: <Orders /> },
       {
         path: "/create-order",
-        element: <h1>This is the order create page</h1>,
+        element: <CreateOrder />,
+      },
+      {
+        path: "/my-profile",
+        element: <MyProfile />,
+      },
+      {
+        path: "/logout",
+        element: <Logout />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
       },
     ],
   },
